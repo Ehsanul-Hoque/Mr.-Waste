6e2b196fa039e7c306fc35e0c1c4265ca6cebc53
Md. Ehsanul Hoque Fahad <ehsanfahad01@gmail.com>
2020-05-29 03:14:17 +0600

    first commit

---
+++ app/src/main/java/io/pantheonsite/alphaoptimus369/mrwaste/MainActivity.java
@@ -0,0 +1,14 @@
+package io.pantheonsite.alphaoptimus369.mrwaste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
